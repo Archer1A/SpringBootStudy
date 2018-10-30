da57e14cbb0ab4ee084f67fe6d385655da2fd4a1
Vic <985695123@qq.com>
2018-10-30 21:25:50 +0800

    my first spring boot project

---
+++ src/main/java/com/hotelhelp/hotelhelp/HotelhelpApplication.java
@@ -0,0 +1,12 @@
+package com.hotelhelp.hotelhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelhelpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hotelhelp/hotelhelp/HotelhelpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hotelhelp.hotelhelp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelhelpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
